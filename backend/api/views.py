9a948b36d5e8251c8d9d1a3b6ebee7fb4f6c28c4
unknown <akbiyik.ilker7@gmail.com>
2022-09-07 22:08:58 +0200

    model added

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+import json
+from products.models import Product
 from django.http import JsonResponse
 # Create your views here.
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "Hello there!"})
+
+    model_data = Product.objects.all().order_by('?').first()
+    data = {}
+    if model_data:
+        data["id"] = model_data.id
+        data["title"] = model_data.title
+        data["content"] = model_data.content
+        data["price"] = model_data.price
+    return JsonResponse(data) 
