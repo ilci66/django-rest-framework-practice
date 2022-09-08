e52197e481460c8a35b2cdd4ffef96cdf43ce457
unknown <akbiyik.ilker7@gmail.com>
2022-09-08 16:57:53 +0200

    return json

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,15 +1,19 @@
 import json
 from products.models import Product
-from django.http import JsonResponse
+from django.forms.models import model_to_dict
+from django.http import JsonResponse, HttpResponse
 # Create your views here.
 
 def api_home(request, *args, **kwargs):
 
     model_data = Product.objects.all().order_by('?').first()
+
     data = {}
+    
     if model_data:
-        data["id"] = model_data.id
-        data["title"] = model_data.title
-        data["content"] = model_data.content
-        data["price"] = model_data.price
+        data = model_to_dict(model_data, fields=['id', 'title', 'price'])
+    
+    data = dict(data)
+    print("data => ", data)
+
     return JsonResponse(data) 
