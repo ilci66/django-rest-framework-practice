5debd61a3e70905617fa9ed1314e8125c126479f
unknown <akbiyik.ilker7@gmail.com>
2022-09-09 22:02:49 +0200

    serializer method

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -3,6 +3,8 @@ from rest_framework import serializers
 from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
+    my_discount = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Product
         fields = [
@@ -10,5 +12,10 @@ class ProductSerializer(serializers.ModelSerializer):
             'content',
             'price',
             'sale_price',
-            'get_discount'
+            'my_discount'
         ]
+
+    # The method will get "get_" in front
+    def get_my_discount(self, obj):
+        print("obj in seralizer => ", obj.id)
+        return obj.get_discount()
